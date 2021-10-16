b35c64f48f742a0d0fa22d7b64d0641f6fcd3e29
Khan-Sohail <ksohail.sk32@gmail.com>
2021-10-16 15:53:18 +0530

    Programs & Program Task Module Done

---
+++ database/migrations/2021_10_16_152044_update_program_tasks_1_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateProgramTasks1Table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('program_tasks', function (Blueprint $table) {
+           $table->string('total_marks',100)->nullable();
+           $table->string('passing_marks',100)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('program_tasks', function (Blueprint $table) {
+            //
+        });
+    }
+}
